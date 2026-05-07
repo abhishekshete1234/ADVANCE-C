246557e966a5ee427232f93aaa62ea6455d95af7
abhishekshete1234 <adityashetepatil@gmail.com>
2026-05-07 13:29:36 +0530

    Add files via upload

---
+++ sum of uapper triangualar.ac.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],r,c,i,j,sum=0;
+	printf("enter column and rows :");
+	scanf("%d%d",&r,&c);
+	
+	printf("enter element :\n");
+	
+	for(i=0; i<r; i++)
+	{
+		for(j=0; j<c; j++)
+		{
+			scanf("%d",&a[i][j]);
+			
+		}
+	}
+	for(i=0; i<r; i++)
+	{
+		for(j=0; j<c; j++)
+		{
+			if(i<=j)
+			sum=sum+a[i][j];
+			
+		}
+	}
+	printf("sum of uapper triangular element :%d",sum);
+}
